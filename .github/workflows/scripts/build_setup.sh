266ab9e6e257127fc3bd78e7079fea7c23ed7c1f
chowder <16789070+chowder@users.noreply.github.com>
2023-07-28 10:38:24 +0100

    Rename bazelisk binary

--- .github/workflows/scripts/build_setup.sh
+++ .github/workflows/scripts/build_setup.sh
@@ -2,4 +2,4 @@ yum install wget -y
 
 wget "https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64" -O bazelisk
 
-mv bazelisk "/usr/local/bin"
+mv bazelisk "/usr/local/bin/bazel"
