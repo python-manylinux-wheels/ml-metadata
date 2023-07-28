ce0ad88709ed51d5aaffa868102ebfac9946e2cb
chowder <16789070+chowder@users.noreply.github.com>
2023-07-28 11:02:48 +0100

    Make bazelisk executable

--- .github/workflows/scripts/build_setup.sh
+++ .github/workflows/scripts/build_setup.sh
@@ -2,4 +2,6 @@ yum install wget -y
 
 wget "https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64" -O bazelisk
 
+chmod +x bazelisk
+
 mv bazelisk "/usr/local/bin/bazel"
